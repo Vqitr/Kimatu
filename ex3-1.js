068d14074690eaf27e60f07f68265ac53e7b1986
Vqitr <131091945+Vqitr@users.noreply.github.com>
2024-01-14 20:15:36 +0900

    Create ex3-1.js

---
+++ ex3-1.js
@@ -0,0 +1,3 @@
+function txtchange1(){document.getElementById('txt1').innerHTML='「ハイウェイ」';}
+function txtchange2(){document.getElementById('txt2').innerHTML='「光るとき」「キャロル」';}
+function txtchange3(){document.getElementById('txt3').innerHTML='「more than words」「深呼吸」';}
