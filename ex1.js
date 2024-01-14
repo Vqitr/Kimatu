240e270aa136f7f38327cb3c40a770e2f8f54d2b
Vqitr <131091945+Vqitr@users.noreply.github.com>
2024-01-14 16:56:17 +0900

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,2 @@
+function imgchange1(){document.getElementById('gazo').src='./image/1ab4f_1770_c65c2db2_e0a08273.jpg';}
+function imgchange2(){document.getElementById('gazo').src='./image/hitsujibungaku_art202204.avif';}
