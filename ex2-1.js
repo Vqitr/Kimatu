7d179419fd9d2ecf00b7c25976d2153e19e110f7
Vqitr <131091945+Vqitr@users.noreply.github.com>
2024-01-14 19:05:30 +0900

    Update ex2-1.js

--- ex2-1.js
+++ ex2-1.js
@@ -1,4 +1,4 @@
 function txtchange1(){document.getElementById('txt1').innerHTML='ギター・ボーカル担当。1996年7月3日生まれの27歳。オリジナル曲の作詞作曲を行っている。';}
 function txtchange2(){document.getElementById('txt2').innerHTML='ベース・コーラス担当。1997年12月24日生まれの26歳。Twitterのメンバー募集に応募し、2017年1月に加入した。';}
-function txtchange3(){document.getElementById('txt3').innerHTML='ドラム・コーラス担当。1997年9月14日生まれの26歳。リーガルリリーというバンドのサポートをした時の動画を見たメンバーにTwitterのDMで誘われ、2015年11月に加入した。';}
+function txtchange3(){document.getElementById('txt3').innerHTML='ドラム・コーラス担当。1997年9月14日生まれの26歳。リーガルリリーのサポートをした時の動画を見たメンバーに誘われ、2015年11月に加入した。';}
   
